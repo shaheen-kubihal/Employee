1c3239e3dead79cca933d4cb92f00c8068e412a1
shaheen-kubihal <shaheenkubihal@gmail.com>
2025-05-06 02:22:24 -0700

    Save the employee details in master

--- Employee.java
+++ Employee.java
@@ -2,18 +2,24 @@ public class Employee {
     String name;
     int id;
     double salary;
-    public Employee(String name, int id, double salary) {
+    long M_no;
+
+    public Employee(String name, int id, double salary, long M_no) {
         this.name = name;
         this.id = id;
         this.salary = salary;
+        this.M_no = M_no;
     }
+
     public void displayinfo() {
         System.out.println("Name: " + name);
         System.out.println("ID: " + id);
         System.out.println("Salary: " + salary);
+        System.out.println("Mobile number: " + M_no); 
     }
+
     public static void main(String[] args) {
-        Employee emp1 = new Employee("XYZ", 101, 50000.0);
+        Employee emp1 = new Employee("XYZ", 101, 50000.0, 1098765098); 
         emp1.displayinfo();
     }
 }
